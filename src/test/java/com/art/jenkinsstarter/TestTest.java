08115104c34c7df3795becb992ec7d60d972d64d
Korben Dallas <ail>
2023-08-03 14:21:29 +0300

    add test

---
+++ src/test/java/com/art/jenkinsstarter/TestTest.java
@@ -0,0 +1,14 @@
+package com.art.jenkinsstarter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void getHello() {
+        System.out.println("Hello from test");
+        assertTrue(true);
+    }
+}
